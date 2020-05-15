07c6cad1ef136bde3882b41908dd1e3aa1f6da75
Natália Freitas <natyff1@hotmail.com>
2020-05-15 19:35:57 -0300

    Cabecalho

--- src/pontuaçao.java
+++ src/pontuaçao.java
@@ -1,9 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class pontuaçao {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		 
+	//CABEÇALHO
+	
+	System.out.println("SISTEMA DE PERFIL DE CLIENTE");
+	System.out.println("--------------------------------");
+	System.out.println("IFORME OS DADOS DO ÚLTIMO ANO");
+	
+		
+		
+		
+		
+		
+		sc.close();
 	}
 
 }
